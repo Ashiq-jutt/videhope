f41061b03f894200cca6f058ebfed1d9515adbd9
Altaf <zalmi6033@gmail.com>
2023-03-18 11:47:14 +0500

    add two api funtion

--- src/utils/api-calls.js
+++ src/utils/api-calls.js
@@ -1,48 +1,62 @@
-import { GetData, GetFormData, PostData, PostFormData, PutData } from "./constant"
+import {
+  GetData,
+  GetFormData,
+  PostData,
+  PostFormData,
+  PutData,
+} from "./constant";
 
 export const login = async (paylaod) => {
-    const res = await PostData('api/staff/login', paylaod);
-    if (res?.data?.succeeded) {
-        localStorage.setItem("token", res?.data?.data?.token);
-        localStorage.setItem("user", JSON.stringify(res?.data?.data?.user));
-
-    }
-    return res;
-}
+  const res = await PostData("api/staff/login", paylaod);
+  if (res?.data?.succeeded) {
+    localStorage.setItem("token", res?.data?.data?.token);
+    localStorage.setItem("user", JSON.stringify(res?.data?.data?.user));
+  }
+  return res;
+};
 export const GetAll = async () => {
-    const res = await GetFormData('api/staff/get-all');
-    return res;
-}
+  const res = await GetFormData("api/staff/get-all");
+  return res;
+};
 export const Register = async (paylaod) => {
-    const res = await PostFormData('api/staff/register', paylaod);
-    if (res?.data?.succeeded) {
-        console.log('successfully Added!')
-    }
+  const res = await PostFormData("api/staff/register", paylaod);
+  if (res?.data?.succeeded) {
+    console.log("successfully Added!");
+  }
 
-    return res;
-}
+  return res;
+};
 export const UpdateStaff = async (paylaod) => {
-    const res = await PutData('api/staff/update-access', paylaod);
-    if (res?.data?.succeeded) {
-        console.log('successfully Added!')
-    }
+  const res = await PutData("api/staff/update-access", paylaod);
+  if (res?.data?.succeeded) {
+    console.log("successfully Added!");
+  }
 
-    return res;
-}
+  return res;
+};
 
 export const GetCustomeServices = async () => {
-    const res = await GetData('api/admin/get-customer-service-counts');
-    return res;
-}
+  const res = await GetData("api/admin/get-customer-service-counts");
+  return res;
+};
 export const GetReportedContent = async () => {
-    const res = await GetData('api/admin/get-reported-content');
-    return res;
-}
+  const res = await GetData("api/admin/get-reported-content");
+  return res;
+};
 export const GetReportedUser = async () => {
-    const res = await GetData('api/admin/get-reported-users');
-    return res;
-}
+  const res = await GetData("api/admin/get-reported-users");
+  return res;
+};
 export const GetChatList = async () => {
-    const res = await GetData('api/admin/get-chat-list');
-    return res;
-}
+  const res = await GetData("api/admin/get-chat-list");
+  return res;
+};
+export const GetRequestType = async (type) => {
+  const res = await GetData(`api/creator-request-/get-requests/${type}`);
+  return res;
+};
+
+export const GetRequestsCount = async () => {
+  const res = await GetData("api/creator-request/get-requests-count");
+  return res;
+};
