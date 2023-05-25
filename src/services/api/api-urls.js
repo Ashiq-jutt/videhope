628a8fc6ff48d29de984c33e9df29a851fa72040
Kamran Khan <75163805+kamran353@users.noreply.github.com>
2023-05-25 10:44:43 +0500

    api url changed

--- src/services/api/api-urls.js
+++ src/services/api/api-urls.js
@@ -1,12 +1,11 @@
-export const IP = 'https://videhope.azurewebsites.net';
+export const IP = "http://kankiraapp-001-site1.etempurl.com";
 export const URLS = {
-    base_url: `https://videhope.azurewebsites.net/api/`,
-    image_url: `${IP}/`,
-    staff: {
-        signup: 'staff/register',
-        login: 'staff/login',
-        get_all: 'staff/get-all',
-        update_access: 'staff/update_access',
-    },
-
+  base_url: `http://kankiraapp-001-site1.etempurl.com/api/`,
+  image_url: `${IP}/`,
+  staff: {
+    signup: "staff/register",
+    login: "staff/login",
+    get_all: "staff/get-all",
+    update_access: "staff/update_access",
+  },
 };
